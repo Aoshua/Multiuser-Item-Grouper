b82e52bb3b0858fb96843dc3aa3cc31668f8f2ff
Tanner Law <tannerlaw@weber.edu>
2020-04-09 19:03:02 -0600

    getting sudo code ready to code my part.

---
+++ MultiuserItemGrouper/MultiuserItemGrouper/Models/Storage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MultiuserItemGrouper.Models
+{
+    public class Storage
+    {
+
+        //private list holding the objects of groups.
+
+        //object for the group
+            //holds object name
+            //owner
+            //list of item object
+
+        //object for the items
+            //holds item name
+            //holds item's text
+
+
+        //create group
+
+        //update group
+
+        //return group's
+
+
+        //create item
+
+        //update item
+
+        //return items for given group ( and info)
+    }
+}
