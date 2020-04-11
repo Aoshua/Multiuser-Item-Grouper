600f018bb394f800b84214552096f1cca8cd5438
Joshua <64.josh@gmail.com>
2020-04-10 20:05:56 -0600

    Sketched some basic UI for the homepage

--- MultiuserItemGrouper/MultiuserItemGrouper/Models/Storage.cs
+++ MultiuserItemGrouper/MultiuserItemGrouper/Models/Storage.cs
@@ -7,8 +7,13 @@ namespace MultiuserItemGrouper.Models
 {
     public static class Storage
     {
-        //private list holding the objects of groups.
         public static List<User> Users { get; set; }
         public static List<Group> Groups { get; set; }
+
+        static Storage()
+        {
+            Users = new List<User>();
+            Groups = new List<Group>();
+        }
     }
 }
