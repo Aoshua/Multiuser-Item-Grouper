4fe6daf3b3e48442cbe15f388983c749f5220030
Joshua <64.josh@gmail.com>
2020-04-14 20:12:13 -0600

    Changed User to UserName

--- MultiuserItemGrouper/MultiuserItemGrouper/Models/user.cs
+++ MultiuserItemGrouper/MultiuserItemGrouper/Models/user.cs
@@ -8,12 +8,10 @@ namespace MultiuserItemGrouper.Models
 		//object for owner
 	public class User // object to keep track of the Users
 	{
-		public int Id { get; set; }//owner id
 
 		public string  Name { get; set; }//owner name
-		public User(string name, int id) //make an owner
+		public User(string name) //make an owner
 		{
-			Id = id;
 			Name = name;
 		}
 	}
