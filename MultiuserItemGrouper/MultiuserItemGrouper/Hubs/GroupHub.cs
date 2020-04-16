87a7c7a1378db6c372cdbec099f683bef28b319c
loganrios <loganrios1@gmail.com>
2020-04-16 14:08:36 -0600

    Build basic server-side hub methods with some associated Manager methods

---
+++ MultiuserItemGrouper/MultiuserItemGrouper/Hubs/GroupHub.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using MultiuserItemGrouper.Models;
+
+namespace MultiuserItemGrouper.Hubs
+{
+    public class GroupHub : Hub
+    {
+
+        public async Task GetGroupNames()
+        {
+            await Clients.Caller.SendAsync("ReturnGroupNames", GroupManager.GetGroupNames());
+        }
+
+        public async Task GetItemsInGroup(string groupName)
+        {
+            string msg = "Return Items In Group";
+            string data = GroupManager.SerializeGroupForUser(groupName, Context.Items["username"].ToString());
+
+            await Clients.Caller.SendAsync("ReturnItemsInGroup", msg, data);
+        }
+
+        public async Task AddItem(string groupName, string itemName, string itemBody)
+        {
+            GroupManager.AddItem(Context.Items["username"].ToString(), groupName, itemName, itemBody);
+            await Clients.All.SendAsync("InEditedGroup", groupName);
+        }
+
+        public async Task LockItem(string groupName, int itemId)
+        {
+            GroupManager.LockItem(Context.Items["username"].ToString(), groupName, itemId);
+            await Clients.All.SendAsync("InEditedGroup", groupName);
+        }
+
+        public async Task UnlockItem(string groupName, int itemId)
+        {
+            GroupManager.UnlockItem(Context.Items["username"].ToString(), groupName, itemId);
+            await Clients.All.SendAsync("InEditedGroup", groupName);
+        }
+
+        public async Task UpdateItem(string groupName, int itemId, string itemName, string itemBody, bool hideItem)
+        {
+            GroupManager.UpdateItem(Context.Items["username"].ToString(),
+                groupName, itemId, itemName, itemBody, hideItem);
+
+            await Clients.All.SendAsync("InEditedGroup", groupName);
+        }
+
+        public async Task DeleteItem(string groupName, int itemId)
+        {
+            GroupManager.DeleteItem(Context.Items["username"].ToString(), groupName, itemId);
+            await Clients.All.SendAsync("InEditedGroup", groupName);
+        }
+
+        public async Task IsInEditedGroup(string groupName)
+        {
+            await GetItemsInGroup(groupName);
+        }
+
+        // todo: not sure this works, must find a way to pass in the client that
+        // performed the illegal operation?
+        public async Task SendErrorMsg(string msg)
+        {
+            await Clients.Caller.SendAsync("ErrorMsg", msg);
+        }
+    }
+}
--- MultiuserItemGrouper/MultiuserItemGrouper/Models/GroupManager.cs
+++ MultiuserItemGrouper/MultiuserItemGrouper/Models/GroupManager.cs
@@ -10,6 +10,12 @@ namespace MultiuserItemGrouper.Models
     {
         public static List<Group> Groups { get; set; }
 
+        public enum CreateGroupResult
+        {
+            FAIL = 0,
+            SUCCESS = 1
+        }
+
         static GroupManager()
         {
             Groups = new List<Group>();
@@ -27,31 +33,73 @@ namespace MultiuserItemGrouper.Models
             return JsonConvert.SerializeObject(GroupNames);
         }
 
-        // Add Item to Group
+        // Create Group, check that name has not already been used
+        public static CreateGroupResult CreateGroup(string name)
+        {
+            foreach (Group group in Groups)
+            {
+                if (name == group.Name)
+                {
+                    return CreateGroupResult.FAIL;
+                }
+            }
 
-        // Remove Item from Group
+            Groups.Add(new Group(name));
+            return CreateGroupResult.SUCCESS;
+        }
 
+        // todo the endless "what if the group doesn't exist?"
+        public static void AddItem(string owner, string groupName, string itemName, string itemBody)
+        {
+            FindGroupByName(groupName).Items.Add(new Item(itemName, itemBody, owner));
+        }
+
+        // todo possibly add owner permission?
+        public static void LockItem(string user, string groupName, int itemId)
+        {
+            FindGroupByName(groupName).Items.Where(i => i.Id == itemId).FirstOrDefault().IsLocked = true;
+        }
+
+        // todo catch possible null? add owner permission?
+        public static void UnlockItem(string user, string groupName, int itemId)
+        {
+            FindGroupByName(groupName).Items.Where(i => i.Id == itemId).FirstOrDefault().IsLocked = false;
+        }
+
+        // todo
+        // Delete Item from Group
+        public static void DeleteItem(string user, string groupName, int itemId)
+        {
+            Group g = FindGroupByName(groupName);
+            Item toDelete = g.Items.Where(i => i.Id == itemId).FirstOrDefault();
+            if (toDelete.Owner == user)
+            {
+                g.Items.Remove(toDelete);
+            }
+
+            // todo should figure out a way to return errors like
+            // "Not owner of item", "Item does not exist"
+        }
+
+        // todo maybe add owner permissions? skipping for now
         // Update Item in Group
+        public static void UpdateItem(string user, string groupName, int itemId, 
+            string itemName, string itemBody, bool hideItem)
+        {
+            
+        }
 
+        // todo
         // Serialize a selected group
         public static string SerializeGroupForUser(string groupName, string user)
         {
-            return ""; // todo
+            // todo edge case for not-found group
+            return JsonConvert.SerializeObject(FindGroupByName(groupName).SerializeGroupForUser(user));
         }
 
+        // todo: null pointer exception if not found??
         private static Group FindGroupByName(string name)
         {
-            // todo: proper implementation for a discardable group (class?)
-            Group selectedGroup = new Group("discardable");
-
-            /*foreach (Group group in Groups)
-            {
-                if (name == group.Name)
-                    selectedGroup = group;
-            }
-
-            return selectedGroup;*/
-
             // This returns the first (and only in this case) group with the exact same name:
             return Groups.Where(g => g.Name == name).FirstOrDefault(); 
         }
