08e57a365c0b20221894f2ff50a009df1f785e82
Joshua <64.josh@gmail.com>
2020-04-16 17:46:19 -0600

    Dyanmically drawing items

--- MultiuserItemGrouper/MultiuserItemGrouper/wwwroot/js/ObjectManager.js
+++ MultiuserItemGrouper/MultiuserItemGrouper/wwwroot/js/ObjectManager.js
@@ -1,19 +1,62 @@
-﻿var allGroups;
+﻿// Global variables:
+var allGroups;
 var selectedGroup;
 var groupItems;
 var username;
 
+// Functions that are called on page load:
+$(function () {
+    setUser();
+    bindGroupsCbo();
+    receiveItems(); // Only calling here for testing
+    drawItems();
+});
+
+// These two functions might just be in comm.js
 function receiveGroups(jString) {
     allGroups = JSON.parse(jString);
 }
-
-function selectGroup() {
-    selectedGroup = $("#cboGroup").val()
-    console.log("Selected group: " + selectedGroup);
+function receiveItems() {
+    groupItems = {
+        "Name": "group1",
+        "Items": [
+            {
+                "Name": "item1",
+                "Body": "Let's pretend that this card really has a lot of text. It was really rainy this morning. I got a lot done today. Work was fun. This thing seems to work so that is nice.",
+                "IsLocked": true,
+                "IsHidden": false
+            },
+            {
+                "Name": "item2",
+                "Body": "more text, this time about item2",
+                "IsLocked": false,
+                "IsHidden": true
+            },
+            {
+                "Name": "Item 3",
+                "Body": "If this goes to next next row, I will be so happy",
+                "IsLocked": false,
+                "IsHidden": true
+            }
+        ]
+    }; // Hardcoding items for now
 }
 
-function receiveGroupItems() {
+function editItem(itemName) {
+    console.log("Edit item: " + itemName);
+}
 
+function deleteItem() {
+
+}
+
+function addGroup(groupName) {
+    console.log("Add group: " + groupName);
+}
+
+// Sets our selected group
+function selectGroup() {
+    selectedGroup = $("#cboGroup").val()
 }
 
 // Retrieves the username from ViewBag:
@@ -29,8 +72,19 @@ function bindGroupsCbo() {
     }
 }
 
-// Functions that are called on page load:
-$(function () {
-    setUser();
-    bindGroupsCbo();
-});
+// Dynamically draws our items
+function drawItems() {
+    var cont = $('#itemsContainer');
+    if (groupItems.Items.length > 0) {
+        for (var i = 0; i < groupItems.Items.length; i++) {
+            if (i % 2 == 0) { // Add a new row if we have an even number (0 based)
+                cont.append("<div class='row'></div>");
+                cont = $('#itemsContainer > div');
+            }
+            cont.append("<div class='col-sm-6'><div class= 'card mar-btm-10' >" +
+                "<div class='card-body'><div class='btn-container' style='float: right;' onclick='editItem('" + groupItems.Items[i].Name + "')'><div class='btn-edit'><i class='fa fa-pencil'></i></div></div><h5 class='card-title'>" +
+                groupItems.Items[i].Name + "</h5><p class='card-text'>" +
+                groupItems.Items[i].Body + "</p></div></div></div>");
+        }
+    }
+}
