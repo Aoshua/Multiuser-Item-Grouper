42a56e7701e94b223615c5ec04dfc69037ade567
Joshua <64.josh@gmail.com>
2020-04-18 10:27:03 -0600

    CRUD functions operable with fake json

--- MultiuserItemGrouper/MultiuserItemGrouper/wwwroot/js/ObjectManager.js
+++ MultiuserItemGrouper/MultiuserItemGrouper/wwwroot/js/ObjectManager.js
@@ -31,15 +31,18 @@ function drawItems() {
                 cont.append("<div class='row'></div>");
                 cont = $('#itemsContainer > div');
             }
-            cont.append(`<div class='col-sm-6'>
+            // Append Card:
+            cont.append(`
+                        <div class='col-sm-6'>
                             <div class= 'card mar-btm-10' >
                                 <div class='card-body'>
-                                    <div class='btn-container' title='Delete Item' style='float: right;' onclick='deleteItem("${groupItems.items[i].name}")'>
+                                    <div class='btn-container' title='Delete Item' style='float: right;' onclick='deleteItem("${groupItems.items[i].id}")'>
                                         <div class='btn-delete'>
                                             <i class='fa fa-times'></i>
                                         </div>
                                     </div>
-                                    <div class='btn-container' title='Edit Item' style='float: right;' onclick='editItem("${groupItems.items[i].name}")'>
+                                    <div id="btnHidden_${groupItems.items[i].id}" data-toggle="modal" data-target="#editItemModal_${groupItems.items[i].id}"></div>
+                                    <div class='btn-container' title='Edit Item' style='float: right;' onclick="showEditItem(${groupItems.items[i].id}, '${groupItems.items[i].name}', '${groupItems.items[i].body}', ${groupItems.items[i].isHidden})" >
                                         <div class='btn-edit'>
                                             <i class='fa fa-pencil'></i>
                                         </div>
@@ -48,7 +51,8 @@ function drawItems() {
                                     <p class='card-text'>${groupItems.items[i].body}</p>
                                 </div>
                             </div>
-                        </div>`);
+                        </div>
+            `);
         }
     }
 }
@@ -59,27 +63,72 @@ function drawItems() {
  * CRUD Functions for  UI:
  *=======================*/
 
-function showAddGroup() {
-
-}
 function addGroup() {
-    
-}
-function showAddItem() {
-
+    var newGroupName = $('#txtNewGroupName').val();
+    //GroupCreated(newGroupName);
 }
 function addItem() {
+    var newItemName = $('#txtItemName').val();
+    var newItemBody = $('#txtItemBody').val();
+    var newItemHidden = $('#chkItemHidden').prop('checked');
+    //AddItem(selectedGroup, newItemName, newItemBody, newItemHidden);
+}
+function showEditItem(itemId, itemName, itemBody, itemHidden) {
+    //LockItem(selectedGroup, itemId); // Lock Item for edit
 
+    // Append Edit Modal:
+    var isHidden = "";
+    if (itemHidden)
+        isHidden = 'checked=""';
+    $('body').append(`
+        <div class="modal fade" id="editItemModal_${itemId}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="itemModalTitle">Add Item</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="form-group">
+                            <label for="txtNewGroupName">Item Name:</label>
+                            <input type="text" class="form-control" id="txtItemName_${itemId}" value="${itemName}">
+                        </div>
+                        <div class="form-group">
+                            <label for="txtItemBody">Item Content:</label>
+                            <textarea class="form-control" id="txtItemBody_${itemId}" rows="3">${itemBody}</textarea>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" value="" id="chkItemHidden_${itemId}" ${isHidden}>
+                            <label class="form-check-label" for="chkItemHidden">
+                                Hidden
+                            </label>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-success" data-dismiss="modal" onclick="editItem(${itemId})">Save</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    `);
+    $('#btnHidden_' + itemId).click(); // Sneakily call my hidden button ;)
 }
-function showEditItem(itemName) {
+function editItem(itemId) {
+    var newItemName = $('#txtItemName_' + itemId).val();
+    var newItemBody = $('#txtItemBody_' + itemId).val();
+    var newItemHidden = $('#chkItemHidden_' + itemId).prop('checked');
 
+    // todo? Remove the modal from body (currently seems like there is no need)
+
+    //UpdateItem(selectedGroup, itemId, newItemName, newItemBody, newItemHidden);
+    //UnlockItem(selectedGroup, itemId);
 }
-function editItem(itemName) {
-    
-}
-function deleteItem(itemName) {
-    if (confirm(`Are you sure you want to delete "${itemName}"?`)) {
-        console.log("Delete item: " + itemName);
+function deleteItem(itemId) {
+    if (confirm(`Are you sure you want to delete "${itemId}"?`)) {
+        //DeleteItem(selectedGroup, itemId);
     }
 }
 
@@ -92,7 +141,6 @@ function deleteItem(itemName) {
 // Sets our selected group:
 function selectGroup() {
     selectedGroup = $("#cboGroup").val();
-    console.log(selectedGroup);
     requestItems(selectedGroup);
     //send selected group to comm.js?
 }
@@ -134,7 +182,7 @@ function requestItems(selectedGroup) {
                 "name": "basket",
                 "body": "A nice, woven basket.",
                 "isLocked": false,
-                "isHidden": false
+                "isHidden": true
             },
             {
                 "id": 3,
@@ -160,6 +208,55 @@ function requestItems(selectedGroup) {
             {
                 "id": 7000,
                 "name": "candy",
+                "body": "Tanner's favorite Reese's Pieces.",
+                "isLocked": true,
+                "isHidden": true
+            },
+            {
+                "id": 2,
+                "name": "basket 2",
+                "body": "A nice, woven basket.",
+                "isLocked": false,
+                "isHidden": false
+            },
+            {
+                "id": 3,
+                "name": "ugly basket 2",
+                "body": "A cheap basket we got on Craigslist.",
+                "isLocked": false,
+                "isHidden": true
+            },
+            {
+                "id": 5,
+                "name": "grapes 2",
+                "body": "Delicious, imported red grapes.",
+                "isLocked": true,
+                "isHidden": false
+            },
+            {
+                "id": 15,
+                "name": "bleu cheese 2",
+                "body": "Josh's favorite Floridian bleu cheese.",
+                "isLocked": false,
+                "isHidden": false
+            },
+            {
+                "id": 7000,
+                "name": "candy 2",
+                "body": "Tanner's favorite Reese's Pieces.",
+                "isLocked": true,
+                "isHidden": true
+            },
+            {
+                "id": 15,
+                "name": "bleu cheese 3",
+                "body": "Josh's favorite Floridian bleu cheese.",
+                "isLocked": false,
+                "isHidden": false
+            },
+            {
+                "id": 7000,
+                "name": "candy 3",
                 "body": "Tanner's favorite Reese's Pieces.",
                 "isLocked": true,
                 "isHidden": true
